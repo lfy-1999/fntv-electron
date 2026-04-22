f55f9cf7965ab3e16859148db7be710b85d243a9
lfy-1999 <131640436+lfy-1999@users.noreply.github.com>
2026-04-22 20:25:54 +0800

    Enable fullscreen mode for the main window
    Set the main window to start in fullscreen mode.

--- src/main/common/mainwin.ts
+++ src/main/common/mainwin.ts
@@ -10,14 +10,15 @@ const mainwinConfig: BrowserWindowConstructorOptions = {
     show: false,
     icon: path.join(__dirname, '../../../build/icon.ico'),
     frame: false,
-    // transparent: true,
+    fullscreen: true, // 【修改】启动即全屏
+    // transparent: true, // 保持注释，如果需要毛玻璃效果可开启，但纯黑背景更稳妥
     webPreferences: {
         webgl: true,
         partition: 'persist:fntv',
         preload: path.join(__dirname, '../../preload/index.js'),
-        nodeIntegration: true,   // 开启 Node.js 支持
-        contextIsolation: false,  // 如果 preload 里要直接改 DOM，通常要关掉
-        spellcheck: false,  // 禁用拼写检查，避免输入法干扰
+        nodeIntegration: true,
+        contextIsolation: false,
+        spellcheck: false,
     }
 };
 
